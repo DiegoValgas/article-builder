4bc69d461fb35f495ccda7d393220e796d4d36f4
Diego Valgas <diego.valgas@gmail.com>
2026-01-06 16:50:38 -0300

    build config

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,12 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+/**
+ * @see https://vite.dev/config/
+ */
 export default defineConfig({
+    publicDir: false,
     plugins: [
         vue(),
         vueDevTools(),
@@ -17,4 +20,18 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         },
     },
+    build: {
+        lib: {
+            entry: {
+                canva: resolve(__dirname, 'src/canva.js'),
+                page: resolve(__dirname, 'src/page.js'),
+            },
+            name: ['Canva', 'Page'],
+            formats: ['es'],
+        },
+        rollupOptions: {
+            external: ['vue'],
+        },
+        cssCodeSplit: true,
+    }
 })
